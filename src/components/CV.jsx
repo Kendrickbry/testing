ae29dd093b3b7a5dba633847fe29dbeb193feebd
Kendrickbry <42410441+Kendrickbry@users.noreply.github.com>
2020-10-13 18:39:15 +0200

    Update CV.jsx

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -7,10 +7,8 @@ return(
     <div className="title-text">
     
       <h1> 
-
-      
       Ich bin Bryam </h1>
-      <img src="/images/bry.jpg"/>
+      <img src="/images/bry.jpg" alt=""/>
       <h2>a programmer.</h2>
     
     </div>
