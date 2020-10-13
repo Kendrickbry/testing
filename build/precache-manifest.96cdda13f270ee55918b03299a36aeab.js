c2fdb72caa6a2fddc4a9d4d38f5c523a20c1cc8a
Kendrickbry <42410441+Kendrickbry@users.noreply.github.com>
2020-10-13 15:28:15 +0200

    iconsForm

---
+++ build/precache-manifest.96cdda13f270ee55918b03299a36aeab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c542f0fe01c1c0fa151fcead6d4427d2",
+    "url": "/testing/index.html"
+  },
+  {
+    "revision": "7ebb57d2e3f20a6916e3",
+    "url": "/testing/static/js/2.17e5c741.chunk.js"
+  },
+  {
+    "revision": "1260a01290ac0c0454e9a7627323041c",
+    "url": "/testing/static/js/2.17e5c741.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d309183a7d1c2607c997",
+    "url": "/testing/static/js/main.92ff5446.chunk.js"
+  },
+  {
+    "revision": "3cfee3d160ce29b2f98c",
+    "url": "/testing/static/js/runtime-main.2bda0bbb.js"
+  }
+]);
